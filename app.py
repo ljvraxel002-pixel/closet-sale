7d1a784bc61ac7f920d410070296306fbba6f009
ljvraxel002-pixel <ljvraxel002@gmail.com>
2026-06-13 13:51:30 -0600

    fix puerto Railway

--- app.py
+++ app.py
@@ -182,4 +182,5 @@ def admin_eliminar(pid):
     return redirect(url_for("admin_panel"))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000))) 
+    
